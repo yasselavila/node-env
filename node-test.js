a912ac4a2c4a63509435a7d7c03f14c8c10a7399
Yassel Avila <yassel.avila@gmail.com>
2016-07-29 19:22:50 -0400

    Script for testing

---
+++ node-test.js
@@ -0,0 +1,11 @@
+/**
+ * yag-env
+ *
+ * @copyright Copyright (c) 2016, Yassel Avila Gil (http://yasselavila.com)
+ * @license   BSD 3 Clause (see LICENSE.txt)
+ * @link      https://github.com/yasselavila/yag-env
+ */
+
+const env = require('./dist');
+
+console.log(env);
